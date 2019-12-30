f5a0ecedee7615d78138d0dd7d571308d7467118
Matheus <matheusdsbarros@gmail.com>
2019-12-30 00:15:55 -0300

    Updates

---
+++ precache-manifest.6ee285c00f89413648ca0be0f67fae2e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d7c0d896f9eb46d4cc0984d64ba0cc",
+    "url": "/Conta-Simples/home/index.html"
+  },
+  {
+    "revision": "1e2400ed4e984ca7102a",
+    "url": "/Conta-Simples/home/static/css/2.439e155c.chunk.css"
+  },
+  {
+    "revision": "cf0d7ab51013dccd5a43",
+    "url": "/Conta-Simples/home/static/css/main.1d71ffc4.chunk.css"
+  },
+  {
+    "revision": "1e2400ed4e984ca7102a",
+    "url": "/Conta-Simples/home/static/js/2.1e1e6153.chunk.js"
+  },
+  {
+    "revision": "f09321e517ab4567bb350ec8faae3b4d",
+    "url": "/Conta-Simples/home/static/js/2.1e1e6153.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf0d7ab51013dccd5a43",
+    "url": "/Conta-Simples/home/static/js/main.68a66360.chunk.js"
+  },
+  {
+    "revision": "c3c15bce391636933c34",
+    "url": "/Conta-Simples/home/static/js/runtime-main.e7c10161.js"
+  },
+  {
+    "revision": "4a4e2d824d2c2dbe770007a8c0f75579",
+    "url": "/Conta-Simples/home/static/media/banner.4a4e2d82.png"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/Conta-Simples/home/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/Conta-Simples/home/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/Conta-Simples/home/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/Conta-Simples/home/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/Conta-Simples/home/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/Conta-Simples/home/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/Conta-Simples/home/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/Conta-Simples/home/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/Conta-Simples/home/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/Conta-Simples/home/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/Conta-Simples/home/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/Conta-Simples/home/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/Conta-Simples/home/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/Conta-Simples/home/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/Conta-Simples/home/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "79beffd653be424349ed5523905ee6b2",
+    "url": "/Conta-Simples/home/static/media/logo-conta-simples.79beffd6.svg"
+  }
+]);
